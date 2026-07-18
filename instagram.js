6ad96500f07fcc86fd7b2054ffb8cef336794353
cockneycards <cockneycards@gmail.com>
2026-07-18 17:35:36 +0100

    Create instagram.js

---
+++ instagram.js
@@ -0,0 +1,18 @@
+const instagramPosts = [
+    {
+        image: "https://res.cloudinary.com/your-cloud/image/upload/post1.jpg",
+        url: "https://www.instagram.com/p/POST1/"
+    },
+    {
+        image: "https://res.cloudinary.com/your-cloud/image/upload/post2.jpg",
+        url: "https://www.instagram.com/p/POST2/"
+    },
+    {
+        image: "https://res.cloudinary.com/your-cloud/image/upload/post3.jpg",
+        url: "https://www.instagram.com/p/POST3/"
+    },
+    {
+        image: "https://res.cloudinary.com/your-cloud/image/upload/post4.jpg",
+        url: "https://www.instagram.com/p/POST4/"
+    }
+];
