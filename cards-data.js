c7f9c362e784439acd7e8e8fe69187bb8dd061c0
cockneycards <cockneycards@gmail.com>
2026-07-28 10:06:59 +0100

    Add Valentines card to card catalogue

--- cards-data.js
+++ cards-data.js
@@ -83,6 +83,7 @@ const CATEGORIES = [
   ["tv-movies", "TV / Movie Cards"],
   ["mothersday-fathersday", "Mothers & Fathers Day Cards "],
   ["seasonal", "Seasonal Cards"],
+  ["valentines", "Valentines"],
 ];
 
 const CARD_CATALOGUE = {
@@ -272,6 +273,8 @@ const CARD_CATALOGUE = {
   "engagement4": { title: "Engagement Card", categories: ["all", "for-her", "weddings-engagements"], preview: "v1784896287/engagement4-preview_vemnd5.png", full: "v1784895840/engagement4_lxnrar.png" },
   "engagement5": { title: "Engagement Card", categories: ["all", "for-her", "weddings-engagements"], preview: "v1784896293/engagement5-preview_ued4t0.png", full: "v1784895841/engagement5_giwzii.png" },
 
+  "valentines1-photo": { title: "Cupids Couple", categories: ["all", "for-her", "valentines"], preview: "v1785229353/valentines-photo1-preview_oqllmb.png", full: "v1785228622/valentines-photo1_ctoshq.png" },
+  
   "wedding1": { title: "Wedding Card", categories: ["all", "for-her", "weddings-engagements"], preview: "v1784896291/wedding1-preview_hxzzks.png", full: "v1784895840/wedding1_ripd1n.png" },
   "wedding2": { title: "Wedding Card", categories: ["all", "for-her", "weddings-engagements"], preview: "v1784896288/wedding2-preview_vir2ip.png", full: "v1784895839/wedding2_q3nzee.png" },
   "wedding3": { title: "Wedding Card", categories: ["all", "for-her", "weddings-engagements"], preview: "v1784896285/wedding3-preview_bwchrr.png", full: "v1784895839/wedding3_sm7ztz.png" },
