d787862c58c47c98d7dbaa0475fcd1668672509c
cockneycards <cockneycards@gmail.com>
2026-08-04 07:44:45 +0100

    Update prints-data.js

--- prints-data.js
+++ prints-data.js
@@ -74,8 +74,8 @@ const PRINT_CATALOGUE = {
         categories: ["all", "sports"],
         preview: "v1784120947/millwall-dad-son_e48hgt.png",
         full: "v1785784030/mfc-dad-son_kh02zr.png",
-        name: { label: "First Name", left: 878, top: 490, startSize: 26, minSize: 20, maxSize: 30, fontFamily: "Anton", fontWeight: "normal", color: "#FFFFFF", canChangeSize: true, canChangeColor: false, canMove: false, canRotate: false, isCurved: true, curveRadius: 150, tiltAngle: 0, label: "Name", maxChars: 12 },
-        age: { label: "Age", left: 880, top: 553, startSize: 70, minSize: 20, maxSize: 120, fontFamily: "Anton", fontWeight: "normal", color: "#FFFFFF", canChangeSize: true, canChangeColor: true, label: "Shirt", maxChars: 2, numericOnly: true, lettersOnly: false, forceUppercase: false, defaultText: "10" },
+        name: { label: "First Name", left: 300, top: 490, startSize: 26, minSize: 20, maxSize: 30, fontFamily: "Anton", fontWeight: "normal", color: "#FFFFFF", canChangeSize: true, canChangeColor: false, canMove: false, canRotate: false, isCurved: true, curveRadius: 150, tiltAngle: 0, label: "Name", maxChars: 12 },
+        age: { label: "Age", left: 300, top: 553, startSize: 70, minSize: 20, maxSize: 120, fontFamily: "Anton", fontWeight: "normal", color: "#FFFFFF", canChangeSize: true, canChangeColor: true, label: "Shirt", maxChars: 2, numericOnly: true, lettersOnly: false, forceUppercase: false, defaultText: "10" },
         name2: { label: "Second Name", left: 400, top: 725, startSize: 48, color: "#FFFFFF" },
         age2: { label: "Age", left: 400, top: 600, startSize: 56, color: "#FFFFFF" }
     },
