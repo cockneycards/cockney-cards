9499e87c6304c9eae65284a40a72ff1ee64fc24d
cockneycards <cockneycards@gmail.com>
2026-08-08 16:35:49 +0100

    Update Christmas Naughty or Nice card details

--- cards-data.js
+++ cards-data.js
@@ -213,8 +213,8 @@ const CARD_CATALOGUE = {
     photo: { left: 909.6, top: 331.3, width: 408.1, height: 385.4 } },
   "christmas-nicest-children": { title: "Santa's Nicest Children", categories: ["christmas"], preview: "v1786197700/christmas-nicest-children-preview_ompran.png", full: "v1786197767/christmas-nicest-children_vglwrb.png",
     photo: { left: 909.6, top: 331.3, width: 408.1, height: 385.4 } },
- "christmas-naughtyornice": { title: "Santa's Naughty or Nice? (Video Card)", categories: ["christmas"], preview: "v1786197700/christmas-kids-video-preview_aujgak.png", full: "v1786203141/santasmessgageqr_jq0qo3.png",
-    qr: { left: 920, top: 700, width: 130, height: 130, url: "https://res.cloudinary.com/uzf4eeky/image/upload/v1786200419/santasmessgageqr_jq0qo3.png" } },
+ "christmas-naughtyornice": { title: "Santa's Naughty or Nice? (Video Card)", categories: ["christmas"], preview: "v1786197700/christmas-kids-video-preview_aujgak.png", full: "v1786197768/christmas-kids-video_sm9nhs.png",
+    qr: { left: 920, top: 700, width: 130, height: 130, url: "https://res.cloudinary.com/uzf4eeky/image/upload/v1786203141/santasmessgageqr_jq0qo3.png" } },
 
 //SPORTS
 
